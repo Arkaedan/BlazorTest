aa36bc4edeef7f44fd0bc2b0e713dfdc692cf512
Samuel Harris <n9702857@qut.edu.au>
2019-10-16 13:50:39 +1000

    Added SpaceX page

---
+++ SpacexLaunch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest
+{
+    public class SpacexLaunch
+    {
+        public string mission_name { get; set; }
+    }
+}
